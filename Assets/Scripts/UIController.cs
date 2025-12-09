6c7101ab853e35851d357f5f4433324c26358587
DankMcTavish <mathewsalewski@gmail.com>
2025-12-09 10:30:52 -0800

    feat: Implement core game management, UI control, and difficulty systems.

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,19 +2,115 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    [Header("Menus")]
+    public GameObject mainMenu;
+    public GameObject pauseMenu;
+    public GameObject gameOverMenu;
+    public GameObject winningScreen;
 
+    [Header("Managers")]
+    public GameManager gameManager;
+    public DifficultyManager difficultyManager;
 
+    private bool isPaused = false;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (gameManager == null) gameManager = FindFirstObjectByType<GameManager>();
+        if (difficultyManager == null) difficultyManager = FindFirstObjectByType<DifficultyManager>();
+
+        ShowMainMenu();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else if (!mainMenu.activeSelf && !gameOverMenu.activeSelf && !winningScreen.activeSelf)
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        winningScreen.SetActive(false);
+        Time.timeScale = 0f; // Ensure game is paused in main menu
+    }
+
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+        gameManager.StartGame();
+    }
+
+    public void ToggleDifficulty()
+    {
+        if (difficultyManager != null)
+        {
+            difficultyManager.NextDifficulty();
+            Debug.Log("Difficulty set to: " + difficultyManager.currentDifficulty);
+            // Ideally update UI text here to show current difficulty
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        gameManager.PauseGame();
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        gameManager.ResumeGame();
+    }
+
+    public void ShowGameOverMenu()
+    {
+        gameOverMenu.SetActive(true);
+    }
+
+    public void RetryGame()
+    {
+        gameOverMenu.SetActive(false);
+        winningScreen.SetActive(false);
+        gameManager.RestartGame();
+    }
+
+    public void ShowWinningScreen()
+    {
+        winningScreen.SetActive(true);
+    }
+
+    public void ContinueToNextLevel()
+    {
+        if (difficultyManager != null)
+        {
+            difficultyManager.NextDifficulty();
+        }
+        winningScreen.SetActive(false);
+        gameManager.RestartGame();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        ShowMainMenu();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Exited");
     }
 }
